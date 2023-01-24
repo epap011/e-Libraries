41e23f9fc93863db373d50c37b5ffe767d1741bd
Efthymis Papageorgiou <58293340+epap011@users.noreply.github.com>
2023-01-24 14:54:35 +0200

    feat(admin-page): showing all library info

--- src/main/webapp/js/admin.js
+++ src/main/webapp/js/admin.js
@@ -2,7 +2,7 @@ function getAllStudents() {
     const xhr = new XMLHttpRequest();
     xhr.onload = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById('operationResult').innerHTML = createTableFromJSON(JSON.parse(xhr.responseText));
+            document.getElementById('operationResult').innerHTML = createTableFromJSON(JSON.parse(xhr.responseText), "user_id");
         }
         else {
             console.log("error");
@@ -15,11 +15,28 @@ function getAllStudents() {
     xhr.send();
 }
 
-function createTableFromJSON(list) {
+function getAllLibrarians() {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById('operationResult').innerHTML = createTableFromJSON(JSON.parse(xhr.responseText), "library_id");
+        }
+        else {
+            console.log("error");
+        }
+    }
+
+    xhr.open("GET", "http://localhost:8080/eLibraries/resource/user/librarian");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send();
+}
+
+function createTableFromJSON(list, idName) {
     let html = "<table><tr><th>ID</th><th>Username</th><th>First Name</th><th>Last Name</th></tr>";
     for (const student in list) {
         html += "<tr><td>" +
-            list[student]['user_id']   + "</td><td>" +
+            list[student][idName]   + "</td><td>" +
             list[student]['username']  + "</td><td>" +
             list[student]['firstname'] + "</td><td>" +
             list[student]['firstname'] + "</td>" +
